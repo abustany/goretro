d38d04d97bd43cab297aa1d1e58f0ddb95917159
Charles <charles.wautier@gmail.com>
2020-05-07 19:32:42 +0200

    Fix unused imports

--- ui-react/src/components/Column.tsx
+++ ui-react/src/components/Column.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 
 import NoteEditor from './NoteEditor'
 import Note from './Note'
--- ui-react/src/components/Note.tsx
+++ ui-react/src/components/Note.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 
 import './Note.scss'
 
