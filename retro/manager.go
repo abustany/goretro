d1501475481631cc4f036408ad6afbd1e862b49f
Charles <charles.wautier@gmail.com>
2020-05-11 00:09:18 +0200

    with manager as well

--- retro/manager.go
+++ retro/manager.go
@@ -22,6 +22,51 @@ type clientInfo struct {
 	retro *Retro
 }
 
+// Commands
+
+type command struct {
+	Name string `json:"name"`
+}
+
+const createRoomCommandName = `create-room`
+
+type createRoomCommand struct {
+	command
+	RoomName string `json:"roomName"`
+}
+
+const joinRoomCommandName = `join-room`
+
+type joinRoomCommand struct {
+	command
+	RoomID string `json:"roomId"`
+}
+
+const identifyCommandName = `identify`
+
+type identifyCommand struct {
+	command
+	Nickname string `json:"nickname"`
+}
+
+const setStateCommandName = `set-state`
+
+type setStateCommand struct {
+	command
+	State uint `json:"state"`
+}
+
+const saveNoteCommentName = `save-note`
+
+type saveNoteCommand struct {
+	command
+	ID   uint   `json:"noteId"`
+	Text string `json:"text"`
+	Mood uint   `json:"mood"`
+}
+
+// Manager
+
 type Manager struct {
 	lock        sync.RWMutex
 	connManager ConnManager
@@ -78,47 +123,6 @@ func (m *Manager) handleDisconnect(clientID sseconn.ClientID) {
 	if clientInfo.retro != nil {
 		m.dispatchEvents(clientInfo.retro.RemoveParticipant(clientID))
 	}
-}
-
-type command struct {
-	Name string `json:"name"`
-}
-
-const createRoomCommandName = `create-room`
-
-type createRoomCommand struct {
-	command
-	RoomName string `json:"roomName"`
-}
-
-const joinRoomCommandName = `join-room`
-
-type joinRoomCommand struct {
-	command
-	RoomID string `json:"roomId"`
-}
-
-const identifyCommandName = `identify`
-
-type identifyCommand struct {
-	command
-	Nickname string `json:"nickname"`
-}
-
-const setStateCommandName = `set-state`
-
-type setStateCommand struct {
-	command
-	State uint `json:"state"`
-}
-
-const saveNoteCommentName = `save-note`
-
-type saveNoteCommand struct {
-	command
-	ID   uint   `json:"noteId"`
-	Text string `json:"text"`
-	Mood uint   `json:"mood"`
 }
 
 func (m *Manager) handleConnectionData(clientID sseconn.ClientID, data json.RawMessage) {
@@ -295,6 +299,8 @@ func (m *Manager) dispatchEvents(events []Event) {
 		}
 	}
 }
+
+//
 
 func moodFromInt(i uint) (Mood, error) {
 	switch i {
