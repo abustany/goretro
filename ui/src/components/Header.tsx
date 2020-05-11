a8233465a58d326eed229589e6b709e6bfadd9e7
Charles <charles.wautier@gmail.com>
2020-05-11 19:59:11 +0200

    Single Line with Flexbox

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import WebGLBanner from './WebGLBanner'
 import './Header.scss'
+import '../stylesheets/utils.scss'
 
 interface Props {
   name?: string;
@@ -13,11 +14,15 @@ export default function Header({name}: Props) {
   let banner;
   if (name || webGLDisabled) {
     banner = <div className="Header">
+      <div className="Header__left Header__side"></div>
 
-      <h1 className="retro-font">Goretro</h1>
+      <div className="Header__title">
+        <h1 className="retro-font">Goretro</h1>
+      </div>
 
-      { name ? <span>with <strong>{name}</strong></span> : <span>Here comes a new challenger!</span> }
-
+      <div className="Header__name Header__side">
+        {name}
+      </div>
     </div>
   } else {
     banner = <WebGLBanner onNotDisplayable={() => { setWebGLDisabled(true) }}/>
