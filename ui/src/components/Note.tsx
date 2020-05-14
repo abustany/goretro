08a101f0cde760a7fe41c9757b215e5506ab3af7
Charles <charles.wautier@gmail.com>
2020-05-14 17:06:48 +0200

    backup

--- ui/src/components/Note.tsx
+++ ui/src/components/Note.tsx
@@ -12,10 +12,9 @@ interface NoteProps {
 
 export default function noteComponent({note, showAuthor, participants}: NoteProps) {
   return <div className='Note' key={note.id}>
-    <p>
-      {note.text}
-    </p>
-    {showAuthor && <em className="Note__Author">
+    {note.text}
+
+    {true && <em className="Note__Author">
       {participants.get(note.authorId)?.name || "Unknown author"}
     </em>}
   </div>
