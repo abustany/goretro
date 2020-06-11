c5f3f685a59cfac3935fd3ec0ab662623bedd063
Charles <charles.wautier@gmail.com>
2020-06-11 20:07:25 +0200

    Remove fontawesome, Redesign clipboard btn

--- ui/src/components/Link.tsx
+++ ui/src/components/Link.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './Link.scss'
+import ClipboardImg from '../images/clipboard.png'
 
 interface Props {
   link: string
 }
 export default function({link}: Props) {
+  const [clicked, setClicked] = useState(false)
+  useEffect(() => {
+    if (clicked) {
+      const id = setTimeout(() => { setClicked(false) }, 1000)
+      return () => clearTimeout(id)
+    }
+  }, [clicked])
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(link)
+    setClicked(true)
+  }
+
   return <div>
     <h2>Invite participants!</h2>
-    <div className="Link" onClick={() => {navigator.clipboard.writeText(link)}}>
-      <i className="fas fa-clipboard Link__icon"></i>
+    <div className="Link" onClick={handleCopy}>
+      <img className="Link__icon" src={ClipboardImg}></img>
+      <span className={`Link__confirmation ${clicked ? '-clicked' : ''}`}>{ clicked ? 'Copied ✓' : 'Copy' }</span>
       <span className="Link__text">{link}</span>
     </div>
   </div>
