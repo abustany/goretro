768654b21e9b24d5f50e0fadc8d4e4127632bbd7
Charles <charles.wautier@gmail.com>
2020-06-29 10:12:32 +0200

    Fix Note action button font-size too small

--- ui/src/components/Note.tsx
+++ ui/src/components/Note.tsx
@@ -55,9 +55,9 @@ export default function({note, author, editable, onNoteSave, tabIndex}: Props) {
 
   const editBadge = () => <button onClick={handleEdit} className="Note__badge Note__edit" data-test-id="noteeditor-edit">✎</button>
   const authorBadge = () => <em className="Note__badge">{ author }</em>
-  const saveBadge = () => <button key="save" onClick={handleSave} className="Note__badge" data-test-id="noteeditor-save"> ✓ </button>
-  const cancelBadge = () => <button key="cancel" onClick={handleCancelEdition} className="Note__badge" data-test-id="noteeditor-cancel"> ✕ </button>
-  const deleteBadge = () => <button key="delete" onClick={handleDelete} className="Note__badge" data-test-id="noteeditor-delete"> ␡ </button>
+  const saveBadge = () => <button key="save" onClick={handleSave} className="Note__badge" data-test-id="noteeditor-save">✓</button>
+  const cancelBadge = () => <button key="cancel" onClick={handleCancelEdition} className="Note__badge" data-test-id="noteeditor-cancel">✕</button>
+  const deleteBadge = () => <button key="delete" onClick={handleDelete} className="Note__badge" data-test-id="noteeditor-delete">␡</button>
 
   // Logic
 
